a168742a36f07b53f4fe932f3bef9f65d68d63e5
gaut <gaut@jpl.nasa.gov>
2021-02-15 15:02:56 -0800

    Prototyped boxed command trait

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,197 @@ use termion::input::TermRead;
 use termion::event::Key;
 use termion::raw::IntoRawMode;
 
+use std::error::Error;
 use std::io;
+
+use clap::App;
+
+const command_names: &'static [&'static str] = &["add", "clear", "help", "generate", "info", "list", "meta", "quit", "remove", "write", "yank"];
+
+pub trait CommandWrapper {
+    fn name(&self) -> &'static str;
+    fn help(&self) -> &'static str;
+    fn parse_and_run(&self);
+}
+
+impl<C: Command> CommandWrapper for C {
+    fn name(&self) -> &'static str {
+        self.name()
+    }
+    fn help(&self) -> &'static str {
+        self.help()
+    }
+    fn parse_and_run(&self) {
+        self.run(self.parse())
+    }
+}
+
+pub trait Command {
+    type Args;
+    fn new() -> Box<dyn CommandWrapper>;
+    fn name(&self) -> &'static str;
+    fn help(&self) -> &'static str;
+    fn run(&self, options: Self::Args);
+    fn parse(&self) -> Self::Args;
+}
+
+pub struct ArgsAdd {
+    show: bool,
+}
+
+pub struct CommandAdd {
+}
+
+impl Command for CommandAdd {
+    type Args = ArgsAdd;
+    fn new() -> Box<dyn CommandWrapper> {
+        Box::new(CommandAdd {})
+    }
+    fn name(&self) -> &'static str { "add" }
+    fn help(&self) -> &'static str { "Add a new entry" }
+    fn run(&self, opts: ArgsAdd) {
+        if opts.show {
+            println!("adding");
+        }
+    }
+    fn parse(&self) -> ArgsAdd {
+        ArgsAdd { show: true }
+    }
+}
+
 
 pub fn confirm_interrupt() -> bool {
     println!("Press ctrl-c or ctrl-d again to quit or any key to continue");
     // need raw mode to read a single character
-    let _stdout = io::stdout().into_raw_mode().unwrap();
-    let b = io::stdin().keys().next().unwrap().unwrap();
-    match b {
-        Key::Ctrl('c') => true,
-        Key::Ctrl('d') => true,
+    match get_key() {
+        Key::Ctrl('c') | Key::Ctrl('d') => true,
         _ => false,
     }
+}
+
+pub fn to_args(input: &str) -> Vec::<&str> {
+    input.split_ascii_whitespace().collect()
+}
+
+pub fn get_key() -> Key {
+    let _stdout = io::stdout().into_raw_mode().unwrap();
+    io::stdin().keys().next().unwrap().unwrap()
+}
+
+pub fn match_command(input: &str) -> Result<&'static str, &'static str> {
+    for name in command_names {
+        if let Some(i) = name.find(input) {
+            if i == 0 {
+                return Ok(name);
+            }
+        }
+    }
+    Err("Unknown command")
+}
+
+
+pub fn edit(prompt: &str) -> String
+{
+    String::from("")
+}
+
+pub enum DbEncryption {
+    Fernet,
+}
+
+pub enum DbFormat {
+    Json,
+}
+
+pub struct Database {
+    encryption: DbEncryption,
+    format: DbFormat,
+    path: String,
+    records: Option<Vec<Record>>,  // Lazy load only when needed
+}
+
+pub struct Record {
+    account: String,
+    username: Option<String>,
+    password: String,
+    notes: Option<String>,
+}
+
+pub fn cmd_add() {
+    println!("add")
+}
+
+pub struct GenerateOpts {
+    length: u32,
+    alphanumeric: bool,
+    show: bool,
+}
+
+pub fn cmd_generate() {
+    println!("generate")
+}
+
+pub struct InfoOpts {
+    show: bool,
+    index: u32,
+}
+
+pub fn cmd_info() {
+    println!("info")
+}
+
+pub struct ListOpts {
+}
+
+pub fn cmd_list() {
+    println!("list")
+}
+
+pub struct MetaOpts {
+    show: bool,
+}
+
+pub fn cmd_meta() {
+    println!("meta")
+}
+
+pub struct RemoveOpts {
+    index: u32,
+}
+
+pub fn cmd_remove() {
+    println!("remove")
+}
+
+pub struct UpdateOpts {
+    account: bool,
+    username: bool,
+    password: bool,
+    notes: bool,
+    show: bool,
+    index: u32,
+}
+
+pub fn cmd_update() {
+    println!("update")
+}
+
+pub struct WriteOpts {
+    encryption: DbEncryption,
+    format: DbFormat,
+    password: bool,
+    show: bool,
+    filepath: String,
+}
+
+pub fn cmd_write() {
+    println!("write")
+}
+
+pub struct YankOpts {
+    index: u32,
+}
+
+pub fn cmd_yank() {
+    println!("yank")
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,34 @@
 use rustyline::error::ReadlineError;
 use rustyline::Editor;
 
-use passrs::confirm_interrupt;
+use passrs;
+use passrs::Command;
+use clap::{Arg, App};
 
-fn main() {
+use std::error::Error;
+
+fn repl() {
     let mut rl = Editor::<()>::new();
     loop {
         let readline = rl.readline("\n\x1b[92;1mpassrs>\x1b[0m ");
         match readline {
             Ok(line) => {
                 rl.add_history_entry(line.as_str());
-                println!("Processing...Done");
+                let words = passrs::to_args(&line);
+                if words.len() > 0
+                {
+                    match passrs::match_command(words[0]) {
+                        Ok("add") => {
+                            let cmd = passrs::CommandAdd::new();
+                            cmd.parse_and_run();
+                        }
+                        Ok(cmd) => println!("Command: {}", cmd),
+                        Err(err) => println!("Error: {}", err),
+                    }
+                }
             },
             Err(ReadlineError::Interrupted) | Err(ReadlineError::Eof) => {
-                if confirm_interrupt() {
+                if passrs::confirm_interrupt() {
                     break
                 }
             },
@@ -23,4 +38,45 @@ fn main() {
             }
         }
     }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let matches = App::new("passrs")
+        .version("0.1.0")
+        .subcommand(App::new("add")
+            .short_flag('A')
+        )
+        .subcommand(App::new("generate")
+            .short_flag('G')
+        )
+        .subcommand(App::new("info")
+            .short_flag('I')
+        )
+        .subcommand(App::new("list")
+            .short_flag('L')
+        )
+        .subcommand(App::new("meta")
+            .short_flag('M')
+        )
+        .subcommand(App::new("remove")
+            .short_flag('R')
+        )
+        .subcommand(App::new("update")
+            .short_flag('U')
+        )
+        .subcommand(App::new("write")
+            .short_flag('W')
+        )
+        .subcommand(App::new("yank")
+            .short_flag('Y')
+        )
+        .get_matches();
+
+    if let Some(matches) = matches.subcommand_matches("add") {
+        passrs::cmd_add();
+    } else {
+        repl();
+    }
+
+    Ok(())
 }
